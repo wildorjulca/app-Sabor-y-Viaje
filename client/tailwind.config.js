b84d573d7ec995e309ec715d2bac54ebd46eae1e
wildorJulcaCachay <julcacachaywildor@gmail.com>
2025-03-20 12:25:43 -0500

    new app client

---
+++ client/tailwind.config.js
@@ -0,0 +1,28 @@
+import {Colors} from './constants/Colors.ts'
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors: {
+        light: {
+          primary: Colors.light.primary,
+          secondary: Colors.light.secondary,
+          tertiary: Colors.light.tertiary,
+          background: Colors.light.background,
+          text: Colors.light.text,
+        },
+        dark: {
+          primary: Colors.dark.primary,
+          secondary: Colors.dark.secondary,
+          tertiary: Colors.dark.tertiary,
+          background: Colors.dark.background,
+          text: Colors.dark.text,
+        },
+      },
+    },
+  },
+  plugins: [],
+}
+
